0dbe4fc22397a7b1a790a3c8ba278916e82a59e6
wangpengcheng <wangpengcheng2018@gmail.com>
2019-10-04 14:55:31 +0800

    first commit

---
+++ GYUS42/cmake-build-debug/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/zhao/Data/AutoD/temp/src/TL740D/include".split(';') if "/home/zhao/Data/AutoD/temp/src/TL740D/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "roscpp;rospy;sensor_msgs;std_msgs".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lTL740D".split(';') if "-lTL740D" != "" else []
+PROJECT_NAME = "TL740D"
+PROJECT_SPACE_DIR = "/home/zhao/Data/AutoD/temp/src/TL740D/cmake-build-debug/devel"
+PROJECT_VERSION = "0.0.0"
---
+++ TL740D/cmake-build-debug/CMakeFiles/TL740D_node.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TL740D_node.
+# This may be replaced when dependencies are built.
